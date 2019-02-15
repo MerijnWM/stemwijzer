9bf03d9ba1c6d3c6c8a7c28d564be701079e9dbb
MerijnWM <merijn.wm@gmail.com>
2019-02-15 10:55:22 +0100

    15-02-2019

--- js/logic.js
+++ js/logic.js
@@ -8,8 +8,10 @@ var statement_title = document.getElementById('statement-title');
 var statement = document.getElementById('statement');
 var statement_ul = document.getElementById('statements-ul');
 var parties_ul = document.getElementById('parties-ul');
-var question_count = 0;
+var statement_count = 0;
 var opinions = [];
+var results = [];
+var filter_results = [];
 
 function loadIntro(){
 	statements.style.display = 'none';
@@ -27,36 +29,37 @@ function loadStatements() {
 	intro.style.display = 'none';
 	back_btn.style.display = 'block';
 	statements.style.display = 'block';
-	statement_title.innerHTML = question_count + 1 + '.' + subjects[question_count].title;
-	statement.innerHTML = subjects[question_count].statement;
-
-	var btn = document.getElementsByClassName('btn')
+	statement_title.innerHTML = statement_count + 1 + '.' + subjects[statement_count].title;
+	statement.innerHTML = subjects[statement_count].statement;
+	
+	//set background to grey of selected choice
+	var btn = document.getElementsByClassName('btn');
 	for (var i = 0; i < btn.length; i++) {
         btn[i].style.backgroundColor = "black";
-    }
-	
-	if(opinions[question_count] != null && opinions[question_count] != ''){
-		var btn = document.getElementById(opinions[question_count]);
+    }	
+
+	if(opinions[statement_count] != null && opinions[statement_count] != ''){
+		var btn = document.getElementById(opinions[statement_count]);
 		btn.style.backgroundColor = 'grey';
 	}
 }
 
 function setChoice(opinion){
-	opinions[question_count] = opinion;
-	if(question_count != 11){
-		question_count += 1; 
+	opinions[statement_count] = opinion;
+	if(statement_count != 11){
+		statement_count += 1; 
 		loadStatements();
 	}else{
-		question_count += 1;
+		statement_count += 1;
 		loadChooseStatements();
 	}		
 }
 
 function back(){
-	if(question_count == 0){
+	if(statement_count == 0){
 		loadIntro();
 	}else{
-		question_count -= 1;
+		statement_count -= 1;
 		loadStatements();
 	}
 }
@@ -67,6 +70,7 @@ function loadChooseStatements(){
 	chooseStatements.style.display = 'block';
 	back_btn.setAttribute("onclick", "back()");
 	
+	//display all statemens
 	if(statement_ul.childNodes.length == 0){
 		for(var index = 0; index < subjects.length; ++index ){		
 			var input = document.createElement("input");
@@ -83,12 +87,13 @@ function loadChooseStatements(){
 }
 
 function loadChooseParties(){
-	console.log(document.querySelectorAll('input[id=statements]'));
+	results = [];
 	chooseStatements.style.display = 'none';
 	score.style.display = 'none';
 	chooseParties.style.display = 'block';
 	back_btn.setAttribute("onclick", "loadChooseStatements()");
 
+	//display all parties
 	if(parties_ul.childNodes.length == 0){
 		for(var index = 0; index < parties.length; ++index ){		
 			var input = document.createElement("input");
@@ -98,6 +103,7 @@ function loadChooseParties(){
 			input.setAttribute("type", "checkbox");	
 			input.setAttribute("value", parties[index].name);
 			input.setAttribute("id", "parties");
+			input.setAttribute("checked", "checked");
 			input.style.display = 'block';			
 			label.innerHTML = parties[index].name;				
 		}
@@ -105,10 +111,70 @@ function loadChooseParties(){
 }
 
 function loadScore(){
+	var score = document.getElementById('score');
+	var points = 0;
+	var parties = document.querySelectorAll('input[id=parties]');
+	var statement_titles = document.querySelectorAll('input[id=statements]');
+
 	chooseParties.style.display = 'none';
 	score.style.display = 'block';
 	back_btn.setAttribute("onclick", "loadChooseParties()");
-	console.log(document.querySelectorAll('input[id=parties]'));
+	
+	//calculate score
+	for(var y = 0; y < opinions.length; ++y ){		
+
+		if(opinions[y] != ""){
+			var sum = 1;
+			points +=1;
+			if(statement_titles[y].checked == true){
+				sum = 2;
+				points +=1;
+			}
+
+			for (var i = 0; i < parties.length; ++i ){		
+				
+				if(opinions[y] == subjects[y].parties[i].position ){
+
+					if(results[subjects[y].parties[i].name] == null){
+						results[subjects[y].parties[i].name] = sum; 
+					}else{
+						results[subjects[y].parties[i].name] += sum;
+					}	
+				}
+			}	
+		}		
+	}	
+
+	// filter result by parties
+	for(var x = 0; x < parties.length; ++x ){
+		if (parties[x].checked == true ) {
+			
+			 filter_results[x] = [parties[x].defaultValue , results[parties[x].defaultValue]];			 
+		}
+	}
+
+	//sort result by best choice
+	var sort_results = filter_results.slice(0);
+	sort_results.sort(function(a,b) {
+	    return  b[1] - a[1];
+	});
+
+	//make div score empty
+	while (score.firstChild) {
+	    score.removeChild(score.firstChild);
+	}
+
+	//display ordered and filtered score 
+	var h2 = document.createElement("h2");
+	score.appendChild(h2);
+	h2.innerHTML = "Uw mening komt het best overeen met:";
+
+	sort_results.forEach(function(sort_result){
+		var p = document.createElement("p");
+		score.appendChild(p);
+		p.innerHTML = sort_result[0] + " " + sort_result[1] + "/" + points;
+	});
 }
+
 
  
